fdeccd57f1d1bcb312ad55b7e8da4e0989009ec3
Andrei-Vidnik <andrei.vidnik@mrsoft.by>
2019-04-16 19:52:47 +0300

    slider moov

--- src/app/components/latest-product-slider/latest-product-slider.component.ts
+++ src/app/components/latest-product-slider/latest-product-slider.component.ts
@@ -13,27 +13,47 @@ export class LatestProductSliderComponent implements OnInit, AfterViewInit {
   public productsList: HTMLCollection;
   public firstElement: ClientRect;
   public lastElement: ClientRect;
+  private moovDiv: HTMLElement;
   get positionMainElement() {
     return this.sliderWrapper.getBoundingClientRect();
   }
-  constructor() { }
+  constructor() {
+    window.onresize = function () {
+      this.init();
+    }.bind(this);
+  }
 
   ngOnInit() {
 
   }
   ngAfterViewInit() {
     window.onload = () => {
+      this.sliderWrapper = this.domSlider.nativeElement;
       this.init();
+      this.setStyleWrapper();
     };
   }
 
   init() {
-    this.sliderWrapper = this.domSlider.nativeElement;
+    this.moovDiv = this.sliderWrapper.querySelector('#sw');
     this.productsList = this.sliderWrapper.getElementsByTagName('app-pre-view-product');
     this.firstElement = this.productsList[0].getBoundingClientRect();
     this.lastElement = this.productsList[this.productsList.length - 1].getBoundingClientRect();
   }
   setStyleWrapper() {
-
+    this.moovDiv.style.transform = 'translateX(0px)';
+    this.moovDiv.style.transitionDuration = '0.9s';
+    this.moovDiv.style.transitionDelay = '0.2s';
+  }
+  nextSlide() {
+    let position = this.parseStyle();
+    this.moovDiv.style.transform = 'translate3d(' + Math.ceil(position - this.firstElement.width) + 'px,' + '0px,' + 100 + 'px' + ')';
+  }
+  parseStyle(): number {
+    return +(this.moovDiv.style.transform.match(/\((-?\d*).*\)/)[1]);
+  }
+  prevSlide() {
+    let position = this.parseStyle();
+    this.moovDiv.style.transform = 'translate3d(' + Math.ceil(position + this.firstElement.width) + 'px,' + '0px,' + 100 + 'px' + ')';
   }
 }
